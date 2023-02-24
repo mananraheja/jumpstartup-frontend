10d9c37f0afd87ceeb2b52905fd60952c118c0cc
Shreya Shinde <shreyarshinde@gmail.com>
2023-02-24 15:41:36 -0500

    Users created

---
+++ frontend-angular/src/app/user/freelancer/freelancer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancer',
+  templateUrl: './freelancer.component.html',
+  styleUrls: ['./freelancer.component.css']
+})
+export class FreelancerComponent {
+
+}

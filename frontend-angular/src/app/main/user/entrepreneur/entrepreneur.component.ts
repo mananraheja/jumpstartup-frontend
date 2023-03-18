50935ac980f57c24f4246f30aa704cb460fd1713
Shreya Shinde <shreyarshinde@gmail.com>
2023-03-18 19:57:44 -0400

    distinguished Users

--- frontend-angular/src/app/main/user/entrepreneur/entrepreneur.component.ts
+++ frontend-angular/src/app/main/user/entrepreneur/entrepreneur.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-entrepreneur',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./entrepreneur.component.css']
 })
 export class EntrepreneurComponent {
+
+constructor(){
+  this.childVar='';
+}
+@Input() childVar: string;
 
 }

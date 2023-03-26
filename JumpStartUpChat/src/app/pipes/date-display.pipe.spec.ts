d1df74261e52d817a085e23af501fa5b59c80cb8
Ujjwal Choudhary <u3choudh@uwaterloo.ca>
2023-03-25 22:07:04 -0400

    Removed from main to move to branch

--- JumpStartUpChat/src/app/pipes/date-display.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateDisplayPipe } from './date-display.pipe';
-
-describe('DateDisplayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateDisplayPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

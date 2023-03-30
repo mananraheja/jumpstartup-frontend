846ef9a66bbdc44ce428e0f19cea532b5ea56697
Shreya Shinde <shreyarshinde@gmail.com>
2023-03-30 19:11:00 -0400

    profile data populated

--- frontend-angular/src/app/main/profile/profile.component.ts
+++ frontend-angular/src/app/main/profile/profile.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
-import { RESTAPIService} from '../restapiservice.service';
+// import { RESTAPIService} from '../restapiservice/.service';
+import { RESTAPIService } from 'src/app/restapiservice.service';
+
 import { Router } from "@angular/router";
 import {firstValueFrom} from 'rxjs';
 
 @Component({
-  selector: 'app-profile-page',
-  templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.css']
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
-export class ProfilePageComponent {
-
+export class ProfileComponent {
+email:string=localStorage.getItem('email')??""
 type:string = localStorage.getItem('type')??""
 uuid : string = localStorage.getItem('uuid')??""
-username:string =''
-email: string=''
+username:string =localStorage.getItem('username')??""
 phone :string=''
 domain : string =''
 institution:string=''
@@ -21,17 +22,34 @@ degree : string=''
 major : string =''
 year: string =''
 work_experience=''
-
-
+fullName=''
 // if (this.type== 'investor') {
 //   return this.service.getInvestorUuid(this.uuid).subscribe
 // }
+constructor (private service: RESTAPIService,private router: Router){
 
+  if (this.type == 'Investor') {
+    this.investor_profile();
+  
+  }
+  
+  else if( this.type=='Entrepreneur'){
+    this.entrepreneur_profile();
+  
+     
+     }
+  
+  else if (this.type=='Freelancer'){
+    this.freelancer_profile();
+    // this.service.getFreelancerUuid(this.uuid).subscribe((body:any)=>{
+    //   console.log(body['uuid'])
+    //  });
+  }
+  }
 async freelancer_profile(){
   await firstValueFrom(this.service.getFreelancerUuid(this.uuid)).then((body: any)=>{
     console.log(body);
-    this.username= body['username']
-    this.email= body['email']
+    // this.username= body['username']
     this.phone= body['phone_number']
     this.domain= body['domain']
     this.institution= body['institution']
@@ -39,14 +57,15 @@ async freelancer_profile(){
     this.major =body['major']
     this.year =body['year_of_completion']
     this.work_experience= body['work_experience']
+    this.fullName= body['firstName']+' '+body['lastName']
   })
 }
 
 async entrepreneur_profile(){
   await firstValueFrom(this.service.getEntrepreneurUuid(this.uuid)).then((body: any)=>{
     console.log(body);
-    this.username= body['username']
-    this.email= body['email']
+    // this.username= body['username']
+    // this.email= body['email']
     this.phone= body['phone_number']
     this.domain= body['domain']
     this.institution= body['institution']
@@ -54,13 +73,15 @@ async entrepreneur_profile(){
     this.major =body['major']
     this.year =body['year_of_completion']
     this.work_experience= body['work_experience']
+    this.fullName= body['firstName']+' '+body['lastName']
+
   })
 }
 async investor_profile(){
   await firstValueFrom(this.service.getInvestorUuid(this.uuid)).then((body: any)=>{
     console.log(body);
-    this.username= body['username']
-    this.email= body['email']
+    // this.username= body['username']
+    // this.email= body['email']
     this.phone= body['phone_number']
     this.domain= body['domain']
     this.institution= body['institution']
@@ -68,28 +89,11 @@ async investor_profile(){
     this.major =body['major']
     this.year =body['year_of_completion']
     this.work_experience= body['work_experience']
+    this.fullName= body['firstName']+' '+body['lastName']
+
   })
 }
-constructor (private service: RESTAPIService,private router: Router){
- 
 
 
-if (this.type == 'Investor') {
-  this.investor_profile();
 
 }
-
-else if( this.type=='Entrepreneur'){
-  this.entrepreneur_profile();
-
-   
-   }
-
-else if (this.type=='Freelancer'){
-  this.freelancer_profile();
-  // this.service.getFreelancerUuid(this.uuid).subscribe((body:any)=>{
-  //   console.log(body['uuid'])
-  //  });
-}
-}
-} 

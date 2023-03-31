f16306547214c10c1b4b7f1389dc100d7e15388e
Harsh  Grover <harshgrover@v1040-wn-rt-a-29-244.campus-dynamic.uwaterloo.ca>
2023-03-31 13:12:28 -0400

    imporve unit test coverage

--- frontend-angular/src/app/main/user/investor/investor.component.ts
+++ frontend-angular/src/app/main/user/investor/investor.component.ts
@@ -11,9 +11,6 @@ import { Router } from "@angular/router";
   styleUrls: ['./investor.component.css']
 })
 export class InvestorComponent {
-//  company_obj = [
-//     { "id": 0, "name": " " },
-// ];
 company_obj=[
   {"entrepreneurUUID": "",
         "company_name": "",
@@ -25,7 +22,7 @@ company_obj=[
         "assets": "",
         "open_to_negotiations": "",
         "profits_in_last_fy": "",
-        "pitch": " "}
+        "pitch": ""}
 ]
   constructor(private service: RESTAPIService,private router: Router){
    
@@ -36,39 +33,13 @@ company_obj=[
   }
   @Input() childVar: string;
 
-
-//   else if (this.type=='Freelancer'){
-//     this.freelancer_profile();
-//     // this.service.getFreelancerUuid(this.uuid).subscribe((body:any)=>{
-//     //   console.log(body['uuid'])
-//     //  });
-//   }
-//   }
-// async freelancer_profile(){
-//   await firstValueFrom(this.service.getFreelancerUuid(this.uuid)).then((body: any)=>{
-//     console.log(body);
-//     // this.username= body['username']
-//     this.phone= body['phone_number']
-//     this.domain= body['domain']
-//     this.institution= body['institution']
-//     this.degree= body['degree']
-//     this.major =body['major']
-//     this.year =body['year_of_completion']
-//     this.work_experience= body['work_experience']
-//     this.fullName= body['firstName']+' '+body['lastName']
-//   })
-// }
-
  i:number=0
 async displayCompanies(){
   await firstValueFrom(this.service.getAllCompanies()).then((body:any)=>{
     let len = body.length
   
     while(this.i<len){
-      // console.log(typeof(body));
-      // this.company_obj[this.i]['id']=body[this.i]['entrepreneurUUID']
-      // this.company_obj[this.i]['name']=body[this.i]['company_name']
-      this.company_obj=body;
+    this.company_obj=body;
     console.log(this.company_obj);
     this.i++;
     }
